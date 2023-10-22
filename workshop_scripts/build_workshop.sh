62840fb1fd1ad9876cb76a6bb55aea404432025f
sofloni <sofloni@gmx.de>
2023-10-22 18:20:26 +0200

    added side quest for applepie of the stegosaurus

--- workshop_scripts/build_workshop.sh
+++ workshop_scripts/build_workshop.sh
@@ -1,18 +1,17 @@
-#!/bin/sh
-
-bash cleanup.sh
-
 #!/bin/bash
-if [ "$#" -ne 1 ] || [ -z "$1"]
+
+if [ "$#" -ne 1 ] || [ -z "$1" ]
 then
   echo "Path to the user_folder_template is missing"
   exit 1
 fi
 
+bash cleanup.sh
+
 python3 dungeon_erzeugung.py $1
 python3 wald_erzeugung.py $1
 python3 huette_platzieren.py $1
 
-cp -v ./bashrc ./hushlogin ./treasurechest ./quest_scripts/quest.sh $1
+cp -v ./.bashrc ./.hushlogin ./treasurechest ./quest_scripts/quest.sh $1
 
 cp -fv ./wizard.cow /usr/share/cowsay/cows/
